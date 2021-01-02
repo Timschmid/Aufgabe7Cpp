53141b97946d6c2520105e3a4368020cd6ae763c
Tim Schmid <timschmid19999@gmail.com>
2021-01-02 13:28:30 +0100

    Aufgabenteil 1

--- main.cpp
+++ main.cpp
@@ -17,4 +17,12 @@ int main() {
     std::cout << "R Mantell-Umfang: " << c.circumference() << std::endl;
     std::cout << "R Volumen: " << c.area() << std::endl;
 
+
+    //Cuboid wäre als Basisklasse eher ungeeignet, da Ein Rechteck nur zwei Wete höhe und Länge benötigt, ein Quader hingegen 3.
+    //Bei der Vererbung ist es jedoch nicht gut/Möglich inter unterklasse Weniger Atribute als in der Basisklasse zu hanben.
+    //Nur Mehr oder gleichviel Attribute als in der Basisklasse sind möglich.
+
+    //Der Typ double ist in diesem Fall geeignet, da somit auch Komma zahlen möglich sind. jedoch sind auch negative zahlen möglich was eher unpraktisch ist.
+    //hier könnte es also jeweils immer ein unsignet int für Zahl und nachkommastellen geben was sinvol sein kann. oder die Üperprüfung auf negatice zahlen.
+
 }
