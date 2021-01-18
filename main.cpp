18615bfaff5fc343ed803ef14d58b8bb744569b4
timschmid <tim.schmid@hs-furtwangen.de>
2021-01-18 16:01:09 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <array>
-#include "Guest.h"
 #include "Employee.h"
 #include "Worker.h"
 #include "Seller.h"
 
 int main() {
-    auto employee = new Employee(4, "Duck", "Tick");
-    std::cout << employee->salary()<<std::endl;
+    Employee* employee = new Worker(42, "Duck", "Tick", 96, 40);
     auto worker = new Worker(4, "Duck", "Tick", 9, 40);
     employee = worker;
     std::cout << employee->salary()<<std::endl;
     auto seller = new Seller(5, "Duck", "Trick", 1000, 2000);
     employee = seller;
     std::cout << employee->salary() << std::endl;
+
+    std::shared_ptr<Employee> unWorker(new Worker(42, "Duck", "Tick", 960, 40));
+    std::cout << unWorker->salary() << std::endl;
 }
 
